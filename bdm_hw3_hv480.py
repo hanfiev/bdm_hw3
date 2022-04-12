9e76ba482810d1938f9d09860a2ecefda31fadb2
Hanfie Vandanu <hanfiev@gmail.com>
2022-04-11 20:15:11 -0400

    Add files via upload

--- bdm_hw3_hv480.py
+++ bdm_hw3_hv480.py
@@ -11,8 +11,9 @@ def main(sc):
   import numpy as np
   import pandas as pd
 
-  kf_rdd = sc.textFile(sys.argv[1] if len(sys.argv)>1 else '/tmp/bdm/keyfood_products.csv', use_unicode=True).cache()
+  spark = SparkSession(sc)
 
+  kf_rdd = sc.textFile('keyfood_products.csv', use_unicode=True).cache()
   reader = csv.reader(open('keyfood_sample_items.csv', 'r'))
   sample_dict = {'upc_code':[],'item_name':[]}
   for row in reader:
@@ -39,14 +40,18 @@ def main(sc):
     foodInsecurityDict[key] = round(data[key]['foodInsecurity']*100)
 
   outputTask1 = kf_rdd.filter(lambda x: not x.startswith('store') and \
-                '-' in x.split(',')[2] and \
-                x.split(',')[2].split('-')[1] in sample_dict['upc_code']) \
-                .map(lambda x: x.split(',')) \
-                .map(lambda x: (x[3], float(x[5].split('\xa0')[0][1:]), int(str(x[0]).replace(str(x[0]),str(foodInsecurityDict[str(x[0])])))))
+                      '-' in x.split(',')[2] and \
+                      x.split(',')[2].split('-')[1] in sample_dict['upc_code']) \
+                      .map(lambda x: x.split(',')) \
+                      .map(lambda x: (x[3], float(x[5].split('\xa0')[0][1:]), int(str(x[0]).replace(str(x[0]),str(foodInsecurityDict[str(x[0])])))))
 
-  outputTask1 = outputTask1.collect()
-  outputTask1.saveAsTextFile(sys.argv[1] if len(sys.argv) > 1 else 'output')
-  
+  outputTask1 = outputTask1.cache()
+  outputTask1.count()
+
+  outputTask1.saveAsTextFile(sys.argv[1] if len(sys.argv)>1 else 'output')
+
+
 if __name__=="__main__":
   sc = SparkContext()
   main(sc)
+
